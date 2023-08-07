cd23a494946a6ba83130489ba9da68ef02b4e533
Juan Chaves <juandev100@gmail.com>
2023-08-07 15:59:22 -0500

    Added choice input in Program, added methods in Board

--- TicTacToe/Board.cs
+++ TicTacToe/Board.cs
@@ -2,7 +2,8 @@
 {
     internal class Board
     {
-        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        private int turn = 1;
 
         public void Show()
         {
@@ -15,6 +16,86 @@
             Console.WriteLine("     |     |      ");
             Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
             Console.WriteLine("     |     |      ");
+        }
+
+        public int CheckWin()
+        {
+            // Horizontal winning condition
+            if (arr[1] == arr[2] && arr[2] == arr[3])
+            {
+                return 1;
+            }
+            else if (arr[4] == arr[5] && arr[5] == arr[6])
+            {
+                return 1;
+            }
+            else if (arr[6] == arr[7] && arr[7] == arr[8])
+            {
+                return 1;
+            }
+
+            // Vertical winning condition
+            else if (arr[1] == arr[4] && arr[4] == arr[7])
+            {
+                return 1;
+            }
+            else if (arr[2] == arr[5] && arr[5] == arr[8])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[6] && arr[6] == arr[9])
+            {
+                return 1;
+            }
+
+            // Diagonal winning condition
+            else if (arr[1] == arr[5] && arr[5] == arr[9])
+            {
+                return 1;
+            }
+            else if (arr[3] == arr[5] && arr[5] == arr[7])
+            {
+                return 1;
+            }
+
+            // Draw?
+            else if (arr[1] != '1' && arr[2] != '2' && arr[3] != '3' && arr[4] != '4' && arr[5] != '5' && arr[6] != '6' && arr[7] != '7' && arr[8] != '8' && arr[9] != '9')
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void SetChoice(int choice, char mark)
+        {
+            if (arr[choice] != 'X' && arr[choice] != 'O')
+            {
+                if (turn % 2 != 0)
+                {
+                    arr[choice] = mark;
+                    turn++;
+                }
+                else
+                {
+                    arr[choice] = mark;
+                    turn++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry the cell {0} is already marked with {1}", choice, arr[choice]);
+                Console.WriteLine("\n");
+                Console.WriteLine("Please wait 2 seconds, board is loading again.....");
+                Thread.Sleep(2000);
+            }
+        }
+
+        public int GetTurn()
+        {
+            return turn;
         }
     }
 }
--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -2,9 +2,33 @@
 {
     class Program
     {
+        static int choice;
+        static char mark;
+
         public static void Main(string[] args)
         {
             Board board = new Board();
+
+            Console.WriteLine("TIC-TAC-TOE");
+            Console.WriteLine("Player 1: X | Player 2: O\n");
+
+            if(board.GetTurn() % 2 != 0)
+            {
+                Console.WriteLine("Player 1 turn\n");
+                mark = 'X';
+            }
+            else
+            {
+                Console.WriteLine("Player 2 turn\n");
+                mark = 'O';
+            }
+
+            board.Show();
+            Console.WriteLine("\n");
+            Console.WriteLine("Your choice?: ");
+            choice = int.Parse(Console.ReadLine());
+            board.SetChoice(choice, mark);
+
             board.Show();
         }
     }
